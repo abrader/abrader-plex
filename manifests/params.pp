7f200b54e147c0308749f4e93a87c1e87cfd7fac
Andrew Brader <abrader@gmail.com>
2014-09-24 23:25:06 +0100

    Moved params to params.pp
    Moved many params that were in server.pp to params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,4 +1,22 @@
 class plex::params {
   $version = '0.9.10.1.585-f31034e'
   #$version = '0.9.9.14.531-7eef8c6'
+
+  $stage_dir = "/tmp/staging"
+
+  case $::osfamily {
+    'RedHat': {
+      $package_name = "${module_name}mediaserver-${version}.${::architecture}.rpm"
+      $package_provider = 'rpm'
+      $package_prereqs = []
+    }
+    'Debian': {
+      $package_name = "${module_name}mediaserver_${version}_${::architecture}.deb"
+      $package_provider = 'dpkg'
+      $package_prereqs = [ 'libnss-mdns', 'avahi-daemon', 'avahi-utils', 'libavahi-common3', 'libavahi-core7', 'libdaemon0', 'dbus', 'libdbus-1-3', 'libsystemd-login0' ]
+    }
+    default: {
+      fail("Unsupported platform: abrader-${module_name} currently doesn't support ${::osfamily} or ${::operatingsystem}")
+    }
+  }
 }
--- manifests/server.pp
+++ manifests/server.pp
@@ -1,30 +1,13 @@
 class plex::server (
   $version = $plex::params::version,
+  $stage_dir = $plex::params::stage_dir,
+  $package_name = $plex::params::package_name,
+  $pacakge_prereqs = $plex::params::package_prereqs,
+  $package_provider = $plex::params::package_provider,
 ) inherits plex::params {
 
-  $stagedir = "/tmp/staging"
-
-  case $::osfamily {
-    'RedHat': {
-      $package_name = "${module_name}mediaserver-${version}.${::architecture}.rpm"
-      $package_provider = 'rpm'
-
-      $package_prereqs = []
-    }
-    'Debian': {
-      $package_name = "${module_name}mediaserver_${version}_${::architecture}.deb"
-      $package_provider = 'dpkg'
-
-      $package_prereqs = [ 'libnss-mdns', 'avahi-daemon', 'avahi-utils', 'libavahi-common3', 'libavahi-core7', 'libdaemon0', 'dbus', 'libdbus-1-3', 'libsystemd-login0' ]
-
-    }
-    default: {
-      fail("Unsupported platform: abrader-${module_name} currently doesn't support ${::osfamily} or ${::operatingsystem}")
-    }
-  }
-
   class {'staging' :
-    path  => $stagedir,
+    path  => $stage_dir,
     owner => 'root',
     group => 'root',
   }
@@ -39,7 +22,7 @@ class plex::server (
 
   package { "${module_name}mediaserver" :
     ensure   => present,
-    source   => "${stagedir}/${module_name}/${package_name}",
+    source   => "${stage_dir}/${module_name}/${package_name}",
     provider => $package_provider,
     require  => [ Staging::File[$package_name], Package[$package_prereqs] ],
   }
